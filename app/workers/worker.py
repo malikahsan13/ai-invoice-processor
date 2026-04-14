6fcfadfa11f6ab26bccf3129ae16674eb188f3af
Malik Ahsan <malikahsan13@gmail.com>
2026-04-14 22:39:49 +0500

    updated worker method

--- app/workers/worker.py
+++ app/workers/worker.py
@@ -1,6 +1,9 @@
 import time
 from app.services.queue import pop_job
-
+import asyncio
+from app.db.db import AsyncSessionLocal
+from app.models.invoice import Invoice
+from sqlalchemy.future import select
 
 def process_job(job):
     print(f"Prossing {job}")
@@ -20,6 +23,22 @@ def start_worker():
         else:
             time.sleep(2)
 
+
+async def update_status(file_id, status, data=None, error=None):
+    async with AsyncSessionLocal() as session:
+        return = await session.execute(
+            select(Invoice).where(Invoice.file_id == file_id)
+        )
+        invoice = result.scaler_one_or_more()
+        
+        if Invoice:
+            invoice.status = status
+            if data:
+                invoice.extracted_data = data
+            if error:
+                invoice.error = str(error)
+                
+            await session.commit()
 
 if __name__ == "__main__":
     start_worker()
